3ad7394234fa21d1e80c88eb3513796ae22c1be3
lenovo <kulinspi@gmail.com>
2021-02-13 12:10:59 +0100

    dwa

---
+++ zabawa.py
@@ -0,0 +1,13 @@
+print("Hello world")
+
+
+
+class TestClass():
+    def __init__(self,first,second):
+        self.first = first
+        self.second = second
+        print(self.first,self.second)
+
+
+
+zmienna = TestClass(10,20)
